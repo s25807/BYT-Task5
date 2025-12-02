2260e63690aecbbfc35b70df18848ba128f2478a
DarkNBolt <mgrombiz@gmail.com>
2025-12-02 22:01:33 +0100

    database.factory-update

--- DesignPattern/DesignPattern/Factory/DatabaseFactory.cs
+++ DesignPattern/DesignPattern/Factory/DatabaseFactory.cs
@@ -2,9 +2,17 @@
 {
     public static class DatabaseFactory
     {
-        //public static IDatabase CreateDatabase(...)
-        //{
-        //  ...
-        //}
+        public static IDatabase CreateDatabase(DatabaseType databaseType)
+        {
+            switch (databaseType)
+            {
+                case DatabaseType.Oracle:
+                    return new OracleDatabase();
+                case DatabaseType.SqlServer:
+                    return new SqlServerDatabase();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
     }
 }
