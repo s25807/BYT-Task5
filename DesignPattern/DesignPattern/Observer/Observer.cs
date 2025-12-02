fae56db603d1b004091e39bee39450718a1e2869
DarkNBolt <mgrombiz@gmail.com>
2025-12-02 22:36:12 +0100

    observer-update

--- DesignPattern/DesignPattern/Observer/Observer.cs
+++ DesignPattern/DesignPattern/Observer/Observer.cs
@@ -4,11 +4,27 @@ namespace DesignPattern.Observer
 {
     public class ConcreteObserver(string userName) : IObserver
     {
+        public readonly string UserName = userName;
+        
+        public void AddSubscriber(ISubject subject)
+        {
+            subject.RegisterObserver(this);
+        }
+
+        public void RemoveSubscriber(ISubject subject)
+        {
+            subject.RemoveObserver(this);
+        }
+        
         // Your code goes here... make sure to add this features:
         //1. Allow to hold the observer's name (e.g. when user Adam wants to observe the subject)
         //2. Creating the Observer
         //3. Registering the Observer with the Subject
         //4. Removing the Observer from the Subject
         //5. Observer will get a notification from the Subject using the following Method
+        public void Update(string availabilty)
+        {
+            Console.WriteLine($"Hi {UserName}, subject status: {availabilty}");
+        }
     }
 }
